0f7c599dbc929f00349407cf5d100e0cf549cf98
Lalit <lalitvijay9480@gmail.com>
2021-07-10 11:55:13 +0530

    fix: page icon

--- components/notion/NotionPage.tsx
+++ components/notion/NotionPage.tsx
@@ -33,7 +33,7 @@ export default function Home({ recordMap, customCss }) {
     const pageIcon = document?.querySelector(".notion-page-icon");
     const link = document.createElement("link");
     link.rel = "icon";
-    link.href = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${pageIcon.innerHTML}</text></svg>`;
+    link.href = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>${pageIcon?.innerHTML}</text></svg>`;
     document.getElementsByTagName("head")[0]?.appendChild(link);
 
     const title = document?.createElement("title");
