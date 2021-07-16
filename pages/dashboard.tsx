18f1f74b0b9442e373a53ffebb7f2e51ac84b086
Lalit <lalitvijay9480@gmail.com>
2021-07-16 22:50:47 +0530

    fix: ts error

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import SiteCard from "@/components/dashboard/SiteCard";
 import { useClerkSWR } from "@/lib/fetcher";
 import { notionSites } from "@prisma/client";
+// @ts-ignore
 import MainLayout from "layouts/mainLayout";
 import Skeleton from "react-loading-skeleton";
 
