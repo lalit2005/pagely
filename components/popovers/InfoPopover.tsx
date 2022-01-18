a141651f58b3919b5a9967635b20be26ca675a83
Lalit <lalitvijay9480@gmail.com>
2022-01-18 11:21:50 +0000

    fix: update radix-ui popover prop

--- components/popovers/InfoPopover.tsx
+++ components/popovers/InfoPopover.tsx
@@ -5,15 +5,17 @@ const InfoPopover: React.FC<{ content: string }> = ({ content, ...props }) => {
   return (
     <span className='relative mx-2 transition-all top-1'>
       <Popover.Root>
-        <Popover.Trigger as='span'>
-          <HiOutlineInformationCircle className='w-5 h-5 p-px rounded cursor-pointer hover:bg-gray-100' />
-        </Popover.Trigger>
-        <Popover.Content sideOffset={10} side='top'>
-          <div className='w-64 px-2 py-1 text-sm bg-black rounded-md shadow-lg text-gray-50'>
-            {content}
-            <div className='mt-3'>{props.children}</div>
-          </div>
-        </Popover.Content>
+        <span>
+          <Popover.Trigger asChild>
+            <HiOutlineInformationCircle className='w-5 h-5 p-px rounded cursor-pointer hover:bg-gray-100' />
+          </Popover.Trigger>
+          <Popover.Content sideOffset={10} side='top'>
+            <div className='w-64 px-2 py-1 text-sm bg-black rounded-md shadow-lg text-gray-50'>
+              {content}
+              <div className='mt-3'>{props.children}</div>
+            </div>
+          </Popover.Content>
+        </span>
       </Popover.Root>
     </span>
   );
